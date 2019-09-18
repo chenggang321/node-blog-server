6644792cb5217c390f1a5ef29877b9b651af8ea6
chenggang <1129137164@qq.com>
2019-09-18 22:36:14 +0800

    配置修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: 'node-blog-server',
-        script: 'app.js',
+        script: './bin/www',
 
         // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
         args: 'one two',
