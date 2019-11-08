d5e54cb7ed700a2798c5372f2ac41d43a1bf8879
chenggang <1129137164@qq.com>
2019-11-08 23:31:58 +0800

    数据库调整

--- config/app.config.js
+++ config/app.config.js
@@ -7,7 +7,7 @@ export const APP = {
 }
 
 export const MONGODB = {
-    uri: `mongodb://127.0.0.1:${argv.dbport || '27017'}/blogDb`,
+    uri: `mongodb://totrip.xin:${argv.dbport || '27017'}/blogDb`,
     username: argv.db_username || 'DB_username',
     password: argv.db_password || 'DB_password',
 }
